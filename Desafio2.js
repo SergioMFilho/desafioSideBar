e3f72469e035738f428b2ecdca23baca19545784
Sergio Medeiros <sergiof11@outlook.com.br>
2022-09-05 15:36:40 -0300

    the button is working

--- Desafio2.js
+++ Desafio2.js
@@ -1,8 +1,15 @@
 function altera_tamanho() {
     x = document.getElementById('div')
-    if(x.style.width == "70px"){
-        x.style.width = 180 + 'px'
-    } else {
+    menuIcon = document.getElementById('teste')
+    if(x.style.width === "180px"){
         x.style.width = 70 + 'px'
+        menuIcon.style.visibility = 'hidden'
+    } else {
+        x.style.width = 180 + 'px'
+
+       setTimeout(() => {
+        menuIcon.style.visibility = 'visible'
+       },500)
     }
+
 }
